e9e65c977f88e9fbf9b163d027177c0013a7407e
Natolium <ent.ko@outlook.jp>
2024-12-08 17:53:30 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,15 @@
-Console.WriteLine("Hello World!!")
+const canvas = document.getElementById("canvas");        
+    let imagePath = "image.png";
+    draw(canvas,imagePath);
+    function draw(canvas,imagePath){
+        console.log("draw");
+        const image = new Image();
+        image.addEventListener("load",function (){
+            canvas.width = image.naturalWidth;
+            canvas.height = image.naturalHeight;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(image, 0, 0);
+            console.log("load!");
+        });
+        image.src = imagePath;
+    }
